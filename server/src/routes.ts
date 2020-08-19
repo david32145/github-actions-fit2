95e38d57a1d3d2f984f488cee18bf4582faed677
david32145 <nascimento32145@gmail.com>
2020-08-19 10:30:06 -0300

    feat(server): add husky and commitlint

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express'
 
 import DeleteTodoController from './app/useCases/DeleteTodo'
-import CreateTodoUseCase from './app/useCases/CreateTodo'
+import CreateTodoController from './app/useCases/CreateTodo'
 import CompleteTodoController from './app/useCases/CompleteTodo'
-import ListTodoUseCase from './app/useCases/ListTodo'
+import ListTodoController from './app/useCases/ListTodo'
 
 const routes = Router()
 
-routes.post('/todos', CreateTodoUseCase.handle)
+routes.post('/todos', CreateTodoController.handle)
 routes.patch('/todos/:id/complete', CompleteTodoController.handle)
-routes.get('/todos', ListTodoUseCase.handle)
+routes.get('/todos', ListTodoController.handle)
 routes.delete('/todos/:id', DeleteTodoController.handle)
 
 export default routes
